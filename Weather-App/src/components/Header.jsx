458a0d1bf8bb1db310bf8796340bace54bc7fdb8
Vector-9 <97702819+Vector-9@users.noreply.github.com>
2023-08-19 02:38:23 +0100

    app setup

---
+++ Weather-App/src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar text-light" style={{ backgroundColor: "#2c1762" }}>
+        <div className=" header container-fluid text-white p-3  fs-4 ">
+          Weather-App
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
