843d9555e2178b0f887726db094562e34389956b
Vector-9 <duzieblaise10@gmail.com>
2023-09-01 22:56:36 +0100

    footer additions

---
+++ Weather-App/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="footer" style={{ backgroundColor: "#210f60" }}>
+      <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 ">
+        <div className="text-white mb-3 mb-md-0">
+          Copyright © 2023. VectorWare Technologies.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
